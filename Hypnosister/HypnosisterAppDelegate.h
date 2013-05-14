371e79135ab6acb43e8ea1858eb09b84db942ac5
Jesus Morales <jesusam@umich.edu>
2013-05-14 08:39:50 -0400

    first project involving subclassing UIScrollView

---
+++ Hypnosister/HypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by Jesus Morales on 5/14/13.
+//  Copyright (c) 2013 Jesus Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
